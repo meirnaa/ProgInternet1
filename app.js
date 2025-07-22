2be77c1ea4b8fef2b0066175578e1a7bec7516a4
meirnaa <144032242+meirnaa@users.noreply.github.com>
2025-07-21 22:30:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 function getById(id) {
     return document.getElementById(id);
 }
-const apiUrl = 'http://localhost:3000/socialifpi/postagem';
+const apiUrl = 'https://socialifpi-emkn.onrender.com/socialifpi/postagem';
 var Reacao;
 (function (Reacao) {
     Reacao["risos"] = "Risos";
